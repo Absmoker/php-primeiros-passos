0a5b99a075a94e2af20995cbad5da75b01f60bed
José Luiz Freire de Macêdo <luizj.201@gmail.com>
2022-01-16 14:41:40 -0300

    condicinal e boleanos

--- projeto1.php
+++ projeto1.php
@@ -1,10 +1,16 @@
 <?php
 
-$idade = 28;
+$idade = 8;
 //$idade +=10 ;
 
 echo $idade . PHP_EOL;
 
-echo gettype($idade) . PHP_EOL;
+//echo "olá mundo! Minha idade é: $idade". PHP_EOL;
 
-echo "olá mundo! Minha idade é: $idade". PHP_EOL;
+echo "Mostre a sua identificação para poder entrar!". PHP_EOL;
+
+if ($idade >= 18 /*or $idade ==18*/) {
+    echo "Você pode entrar." . PHP_EOL;
+}else{
+        echo "Você tem $idade.\nVocê não pode entrar!". PHP_EOL; 
+}
