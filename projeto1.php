683bcd1e176db1f1718b03be7c03e234c10591d8
José Luiz Freire de Macêdo <luizj.201@gmail.com>
2022-01-16 10:56:03 -0300

    variaveis e seus tipos

--- projeto1.php
+++ projeto1.php
@@ -1,10 +1,10 @@
 <?php
 
-echo "\nolá mundo!\n\n";
-
 $idade = 28;
-$idade +=10 ;
+//$idade +=10 ;
 
 echo $idade;
 echo "\n";
 echo gettype($idade);
+
+echo "\nolá mundo! Minha idade é: $idade\n";
