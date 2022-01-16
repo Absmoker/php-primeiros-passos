fbc5d1aa5d6a85d6e9ab2d94e2cc4a3fec4bd00c
José Luiz Freire de Macêdo <luizj.201@gmail.com>
2022-01-16 14:59:59 -0300

    condições duplas

--- projeto1.php
+++ projeto1.php
@@ -1,16 +1,20 @@
 <?php
 
 $idade = 8;
+$acompanhante = 1;
 //$idade +=10 ;
 
 echo $idade . PHP_EOL;
 
 //echo "olá mundo! Minha idade é: $idade". PHP_EOL;
 
-echo "Mostre a sua identificação para poder entrar!". PHP_EOL;
+echo "Se você for maior de 18, ou se você estiver acompanhado". PHP_EOL;
 
-if ($idade >= 18 /*or $idade ==18*/) {
+if ($idade >= 18 or ($idade <18 and $acompanhante>1)) {
     echo "Você pode entrar." . PHP_EOL;
+    echo "Divirta-se!";
 }else{
         echo "Você tem $idade.\nVocê não pode entrar!". PHP_EOL; 
+        echo "Retire-se da fila!";
 }
+echo PHP_EOL;
