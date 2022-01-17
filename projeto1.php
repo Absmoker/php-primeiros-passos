34d7ff3b295083c23ddc7cdfbe96e587f779e5d4
José Luiz Freire de Macêdo <luizj.201@gmail.com>
2022-01-17 10:27:40 -0300

    aprendendo o while

--- projeto1.php
+++ projeto1.php
@@ -1,23 +1,29 @@
 <?php
 
-$idade = 16;
-$acompanhante = 2;
-//$idade +=10 ;
+// $idade = 16;
+// $acompanhante = 2;
+// //$idade +=10 ;
 
-echo $idade . PHP_EOL;
+// echo $idade . PHP_EOL;
 
-//echo "olá mundo! Minha idade é: $idade". PHP_EOL;
+// //echo "olá mundo! Minha idade é: $idade". PHP_EOL;
 
-echo "Você so pode entrar se for maior de 18.\nOu se for maior de 16 e estiver acompanhado". PHP_EOL;
+// echo "Você so pode entrar se for maior de 18.\nOu se for maior de 16 e estiver acompanhado". PHP_EOL;
 
-if ($idade >= 18) {
-    echo "Você pode entrar." . PHP_EOL;
-    echo "Divirta-se!";    
-}else if($idade >=16 and $acompanhante >1){
-    echo "você tem $idade, e está acompanhado (a), Pode entrar\n";
-    echo "Divirta-se!";
-}else{
-    echo "Você tem $idade.\nVocê não pode entrar!". PHP_EOL; 
-    echo "Retire-se da fila!";
+// if ($idade >= 18) {
+//     echo "Você pode entrar." . PHP_EOL;
+//     echo "Divirta-se!";    
+// }else if($idade >=16 and $acompanhante >1){
+//     echo "você tem $idade, e está acompanhado (a), Pode entrar\n";
+//     echo "Divirta-se!";
+// }else{
+//     echo "Você tem $idade.\nVocê não pode entrar!". PHP_EOL; 
+//     echo "Retire-se da fila!";
+// }
+// echo PHP_EOL;
+
+$contadorI = 1;
+while($contadorI <= 15){
+    echo "$contadorI" . PHP_EOL;
+    $contadorI +=1;
 }
-echo PHP_EOL;
